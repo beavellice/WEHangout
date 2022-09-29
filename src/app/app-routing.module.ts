d72ada0a8eb81027ed0c1289b5ea78347c1ae0aa
beavellice <beatrice.trivellone@student.univaq.it>
2022-09-29 18:24:48 +0200

    prova push template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,52 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignupPageModule)
+  },
+  {
+    path: 'seguiti',
+    loadChildren: () => import('./pages/seguiti/seguiti.module').then(m => m.SeguitiPageModule)
+  },
+  {
+    path: 'eventi',
+    loadChildren: () => import('./pages/eventi/eventi.module').then( m => m.EventiPageModule)
+  },
+  {
+    path: 'event-list',
+    loadChildren: () => import('./pages/event-list/event-list.module').then( m => m.EventListPageModule)
+  },
+  {
+    path: 'aggiungi-evento',
+    loadChildren: () => import('./pages/aggiungi-evento/aggiungi-evento.module').then( m => m.AggiungiEventoPageModule)
+  },
+  {
+    path: 'detail-poi',
+    loadChildren: () => import('./pages/detail-poi/detail-poi.module').then( m => m.DetailPoiPageModule)
+  },
+  {
+    path: 'luoghi',
+    loadChildren: () => import('./pages/luoghi/luoghi.module').then( m => m.LuoghiPageModule)
+  },
+  {
+    path: 'profilo',
+    loadChildren: () => import('./pages/profilo/profilo.module').then( m => m.ProfiloPageModule)
+  },
+  {
+    path: 'profilo-public',
+    loadChildren: () => import('./pages/profilo-public/profilo-public.module').then( m => m.ProfiloPublicPageModule)
   },
 ];
 
